479815be773d82ca7adfe54cea491a39c6696438
Joe Greubel <joegreubel1@gmail.com>
2017-01-28 16:40:53 -0800

    Initial Commit

---
+++ CopyPasta/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CopyPasta
+//
+//  Created by Joe Greubel on 1/28/17.
+//  Copyright © 2017 Joe Greubel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
